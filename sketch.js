7a573197e35cc1842e31763a2d880620ee8442bf
killer <73597961+killer1101@users.noreply.github.com>
2021-03-18 12:30:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Body = Matter.Body;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=200; 
+var score =0;
+
+function setup() {
+  createCanvas(500,700);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20)
+
+  for (var k = 0; k <=width; k = k + 80) {
+		divisions.push(new Division(k, height-divisionHeight/2, 10, divisionHeight));
+	  }
+  
+    for(var I= 75; I <= 375; I=I+100)
+    {
+         
+      
+        for(var j = 40; j <= width; j=j+50)
+        {
+          plinkos.push(new Plinko(j,I));
+          
+        }
+        
+    }
+  
+}
+
+function draw() {
+  background(0);  
+ Engine.update(engine);
+  for (var k = 0; k < divisions.length; k++) {
+     
+    divisions[k].display();
+    }
+
+  for(var j = 0; j < plinkos.length; j++ ){
+
+    plinkos[j].display();
+  }
+
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+    score++;
+    }
+  
+   for (var j = 0; j < particles.length; j++) {
+    
+    particles[j].display();
+    }
+  }
